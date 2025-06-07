48c6d515a4c5c1ff0d976cd471d5b50b4420b946
TheOneGoofAli <alilesha552@gmail.com>
2025-06-07 03:41:42 +0300

    Set up base of mod, localization + Jaw-some
    This sets up the base of the mod as the main script includes assignment of variables and loading of items in a given folder path.
    
    Also contains submission for Jaw-some Joker as proposed in the Google Docs document.

---
+++ localization/default.lua
@@ -0,0 +1,13 @@
+return {
+	descriptions = {
+		Joker = {
+			j_flor_jawsome = {
+				name = 'Jaw-some',
+				text = {
+					"When a {C:attention}Blind is {C:attention}skipped{}, an {C:dark_edition}Edition{}",
+					"is applied to a {C:attention}random Joker{}",
+				}
+			},
+		}
+	}
+}
