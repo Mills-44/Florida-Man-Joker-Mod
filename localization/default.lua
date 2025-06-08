a527738805a336c0e63fd9fdfbd703e7e29e3de5
CGNik <76545649+CG-Nik@users.noreply.github.com>
2025-06-07 20:36:47 -0500

    Update default.lua

--- localization/default.lua
+++ localization/default.lua
@@ -8,6 +8,62 @@ return {
 					"is applied to a {C:attention}random Joker{}",
 				}
 			},
+			j_flor_10ToeJimbo = {
+				name = '10 Toe Jimbo',
+				text = {
+					"Gain {C:money}$#1#{} if the",
+				        "played hand is {C:attention}On Fire{}",
+				        "{s:0.5} {}",
+					"{B:1,C:white,s:0.75,E:1}Code: CGNik",
+				        "{B:1,C:white,s:0.75,E:1}Art: -----",
+					"{B:1,C:white,s:0.75,E:1}Idea: InfamousInvictis"
+				}
+			},
+			j_flor_AmusingPark = {
+				name = "Amusing Park",
+			        text = {
+				        "Gains {X:mult,C:white}X#1#{} Mult per {C:attention}unique{} poker hand played this {C:attention}Ante{}",
+				        "{C:inactive}(Currently {}{X:mult,C:white}X#2#{}{C:inactive} Mult){}",
+				        "{C:inactive}(Poker Hands played: #4#){}",
+				        "{s:0.5} {}",
+				        "{B:1,C:white,s:0.75,E:1}Code: CGNik",
+				        "{B:1,C:white,s:0.75,E:1}Art: -----",
+			            	"{B:1,C:white,s:0.75,E:1}Idea: InfamousInvictis"
+			        },
+			},
+			j_flor_RayBanned = {
+				name = "Ray Banned",
+			        text = {
+				        "Played {C:hearts}Hearts{} and {C:diamonds}Diamonds{} turn into",
+				        "{C:spades}Spades{} and {C:clubs}Clubs{} respectively",
+				        "{s:0.5} {}",
+				        "{B:1,C:white,s:0.75,E:1}Code: CGNik",
+				        "{B:1,C:white,s:0.75,E:1}Art: -----",
+				        "{B:1,C:white,s:0.75,E:1}Idea: InfamousInvictis"
+			        },
+			},
+			j_flor_RedHanded = {
+				name = "Red Handed",
+			        text = {
+				        "Played {C:spades}Spades{} and {C:clubs}Clubs{} turn into",
+				        "{C:hearts}Hearts{} and {C:diamonds}Diamonds{} respectively",
+				        "{s:0.5} {}",
+				        "{B:1,C:white,s:0.75,E:1}Code: CGNik",
+				        "{B:1,C:white,s:0.75,E:1}Art: -----",
+				        "{B:1,C:white,s:0.75,E:1}Idea: InfamousInvictis"
+			        },
+			},
+			j_flor_SunburntSam = {
+				name = "Sunburnt Sam",
+			        text = {
+				        "{X:mult,C:white}X#1#{} Mult if all cards {C:attention}held in hand{}",
+				        "are {C:hearts}Hearts{} or {C:diamonds}Diamonds{}",
+				        "{s:0.5} {}",
+				        "{B:1,C:white,s:0.75,E:1}Code: CGNik",
+				        "{B:1,C:white,s:0.75,E:1}Art: gfsgfsgfs",
+				        "{B:1,C:white,s:0.75,E:1}Idea: InfamousInvictis"
+			        },
+			},
 		}
 	}
 }
