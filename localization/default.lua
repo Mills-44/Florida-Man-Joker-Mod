06c16be6950969687a19241fb518a8e47d621c79
Mills-44 <mylesbrodsky44@gmail.com>
2025-06-08 21:11:38 -0500

    Update default.lua

--- localization/default.lua
+++ localization/default.lua
@@ -75,6 +75,16 @@ return {
 				        "{B:1,C:white,s:0.75,E:1}Idea: InfamousInvictis"
 			        },
 			},
+			j_flor_liftoff = {
+				name = 'Liftoff!',
+				text = {
+					"Scoring {C:attention}A to 5 Straight's{}",
+					"Increases money by {C:gold}$1{}",
+					"when played hands score",
+					"{C:inactive}Total:{}{C:gold} $#1#",
+					"{C:inactive}Resets Every Boss Blind"
+				}
+			},
 		}
 	}
 }
